41af45c21a37ad65d36dc588c318322d018a3a78
Mateusz Grzeskowiak <mateusz.r.grzeskowiak@gmail.com>
2020-07-12 12:54:08 +0200

    Done on and off

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,35 @@
 import com.sda.dp.Kitchen;
+import com.sda.dp.KitchenDevice;
+import com.sda.dp.adapters.CoffeeMachineAdapter;
+import com.sda.dp.adapters.FridgeAdapter;
+import com.sda.dp.adapters.OvenAdapter;
+import com.sda.dp.devices.CoffeMachine;
+import com.sda.dp.devices.Fridge;
+import com.sda.dp.devices.Oven;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Kitchen kitchen = new Kitchen();
-        kitchen.turnEverythingOff();
+        List<KitchenDevice> kitchenDevices = new ArrayList<>();
+        kitchenDevices.add(new CoffeeMachineAdapter(new CoffeMachine()));
+        kitchenDevices.add(new OvenAdapter(new Oven()));
+        kitchenDevices.add(new FridgeAdapter(new Fridge()));
+
+        turnAllDevicesOn(kitchenDevices);
+
+        turnAllDevicesOff(kitchenDevices);
+    }
+
+    private static void turnAllDevicesOn(List<KitchenDevice> kitchenDevices) {
+        for (KitchenDevice device : kitchenDevices) {
+            device.turnDeviceOn();
+        }
+    }
+    private static void turnAllDevicesOff(List<KitchenDevice> kitchenDevices) {
+        for (KitchenDevice device : kitchenDevices) {
+            device.turnDeviceOff();
+        }
     }
 }
